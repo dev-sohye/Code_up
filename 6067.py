6a6266569f1cb6cf29c5cb96adc84aacd6337301
dev_yomi <yojolady48@gmail.com>
2021-04-24 16:24:06 +0900

    [기초-조건/선택실행구조] 정수 1개 입력받아 분류하기

---
+++ 6067.py
@@ -0,0 +1,12 @@
+num = int(input())
+
+if num >= 0:
+    if num % 2 == 0:
+        print ("C")
+    else:
+        print ("D")
+else:
+    if num % 2 == 0:
+        print ("A")
+    else:
+        print ("B")

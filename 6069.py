f778812d1a4bf3838bc980d22ec03d2f887c7a36
dev_yomi <yojolady48@gmail.com>
2021-04-25 22:18:37 +0900

    [기초-조건/선택실행구조] 평가 입력받아 다르게 출력하기

---
+++ 6069.py
@@ -0,0 +1,12 @@
+talk = input()
+
+if talk == "A":
+    print ("best!!!")
+elif talk == "B":
+    print ("good!!")
+elif talk == "C":
+    print ("run!")
+elif talk == "D":
+    print ("slowly~")
+else:
+    print ("what?")
